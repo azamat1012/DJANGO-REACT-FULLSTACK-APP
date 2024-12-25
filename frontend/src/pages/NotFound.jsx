c11965bb0bc70a52fa2ef4f4f46a233ae14827c2
Azamat Abdibaetov <aza2004mat001@gmail.com>
2024-12-25 09:58:53 +0500

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+    return <div>
+        <h1>404 Страница не найдена</h1>
+        <p>Страница, которую Вы искали не существует :с</p>
+    </div>
+}
+
+export default NotFound
